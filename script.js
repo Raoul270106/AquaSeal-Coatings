f07d0129f10743f1441d914e8ecb4514c6c094c7
Raoul270106 <raoul.raju@hotmail.com>
2025-08-04 15:19:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Scroll smooth behavior for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').slice(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
